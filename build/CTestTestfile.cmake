d46d0e6ce5974ed3e7830dfe21c4a28ba70a0e6a
dongfang20101113 <dongfang20101113@gmail.com>
2026-09-25 06:12:17 +0800

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,24 @@
+# CMake generated Testfile for 
+# Source directory: D:/codes/dchat
+# Build directory: D:/codes/dchat/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test("protocol_tests" "D:/codes/dchat/build/test_protocol.exe")
+set_tests_properties("protocol_tests" PROPERTIES  _BACKTRACE_TRIPLES "D:/codes/dchat/CMakeLists.txt;109;add_test;D:/codes/dchat/CMakeLists.txt;0;")
+add_test("render_tests" "D:/codes/dchat/build/test_render.exe")
+set_tests_properties("render_tests" PROPERTIES  _BACKTRACE_TRIPLES "D:/codes/dchat/CMakeLists.txt;110;add_test;D:/codes/dchat/CMakeLists.txt;0;")
+add_test("rounded_tests" "D:/codes/dchat/build/test_rounded.exe")
+set_tests_properties("rounded_tests" PROPERTIES  _BACKTRACE_TRIPLES "D:/codes/dchat/CMakeLists.txt;111;add_test;D:/codes/dchat/CMakeLists.txt;0;")
+add_test("bubble_tests" "D:/codes/dchat/build/test_bubble.exe")
+set_tests_properties("bubble_tests" PROPERTIES  _BACKTRACE_TRIPLES "D:/codes/dchat/CMakeLists.txt;112;add_test;D:/codes/dchat/CMakeLists.txt;0;")
+add_test("image_preview_tests" "D:/codes/dchat/build/test_image_preview.exe")
+set_tests_properties("image_preview_tests" PROPERTIES  _BACKTRACE_TRIPLES "D:/codes/dchat/CMakeLists.txt;113;add_test;D:/codes/dchat/CMakeLists.txt;0;")
+add_test("history_tests" "D:/codes/dchat/build/test_history.exe")
+set_tests_properties("history_tests" PROPERTIES  _BACKTRACE_TRIPLES "D:/codes/dchat/CMakeLists.txt;114;add_test;D:/codes/dchat/CMakeLists.txt;0;")
+add_test("server_command_tests" "D:/codes/dchat/build/test_server_command.exe")
+set_tests_properties("server_command_tests" PROPERTIES  _BACKTRACE_TRIPLES "D:/codes/dchat/CMakeLists.txt;115;add_test;D:/codes/dchat/CMakeLists.txt;0;")
+add_test("auth_tests" "D:/codes/dchat/build/test_auth.exe")
+set_tests_properties("auth_tests" PROPERTIES  _BACKTRACE_TRIPLES "D:/codes/dchat/CMakeLists.txt;116;add_test;D:/codes/dchat/CMakeLists.txt;0;")
+add_test("file_transfer_tests" "D:/codes/dchat/build/test_file_transfer.exe")
+set_tests_properties("file_transfer_tests" PROPERTIES  _BACKTRACE_TRIPLES "D:/codes/dchat/CMakeLists.txt;117;add_test;D:/codes/dchat/CMakeLists.txt;0;")
